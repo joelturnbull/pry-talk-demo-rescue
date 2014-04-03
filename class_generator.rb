5d7151ac832ee08bfca2e5bd266bb6c1d9be361a
Joel Turnbull <joelbywan@gmail.com>
2014-04-03 14:52:15 -0400

    class generator v2

--- class_generator.rb
+++ class_generator.rb
@@ -1,28 +1,14 @@
 require 'pry'
-require 'active_support/inflector'
 
-# given a class name "BowlingGame"
-# create a file bowling_game.rb 
-# write a class definition to it
+# Given a class name like "BowlingGame"
+# Create a file bowling_game.rb 
+# And write a class definition to it like:
 # class BowlingGame
 #
 # end
-
-def file_name_for_class(klass)
-  "#{klass.underscore}.rb"
-end
-
-def class_definition_for_class(klass)
-  "class #{klass}\n\nend"
-end
-
-def create_class(file_name,class_definition)
-  File.write(file_name,class_definition)
-end
 
 klass = ARGV[0]
 
 file_name = file_name_for_class(klass)
 class_definition = class_definition_for_class(klass)
 create_class(file_name,class_definition)
-
