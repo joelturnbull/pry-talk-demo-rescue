2778283a70f5a49dc36e7b80c80a259b7b2f2b4f
Joel Turnbull <joelbywan@gmail.com>
2014-04-03 14:17:04 -0400

    add active_support and use it

--- class_generator.rb
+++ class_generator.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'active_support/inflector'
 
 # given a class name "BowlingGame"
 # create a file bowling_game.rb 
@@ -8,6 +9,7 @@ require 'pry'
 # end
 
 def create_file_for_class(klass)
+  file_name = "#{klass.underscore}.rb"
   binding.pry
 end
 
