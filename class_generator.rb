89ff776370f995749faee9572bc0b7ac0103ae38
Joel Turnbull <joelbywan@gmail.com>
2014-04-03 11:57:14 -0400

    initial class generator

---
+++ class_generator.rb
@@ -0,0 +1,11 @@
+# given a class name "BowlingGame"
+# create a file bowling_game.rb 
+# write a class definition to it
+# class BowlingGame
+# end
+
+klass = "BowlingGame"
+
+file = create_file_for_class(klass)
+write_class_definition(file,klass)
+
